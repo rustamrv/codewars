49147c1b8a0a7f262fb63293b73391b414a12339
Rustam <rystamrv@gmail.com>
2019-01-12 18:14:42 +0200

    Update alphabet_position.py

--- alphabet_position.py
+++ alphabet_position.py
@@ -1,3 +1,8 @@
+#In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+#If anything in the text isn't a letter, ignore it and don't return it.
+#"a" = 1, "b" = 2, etc.
+
+
 def alphabet_position(text):
     output = ""
     for ch in text:
